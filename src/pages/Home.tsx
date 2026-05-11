3ae0a66e9c3999369b9674f2ef22656012fae7cb
alxyiko <alyumikogarcia@gmail.com>
2026-05-11 14:54:47 +0800

    added scubacat.gif in home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-800 text-white">
-      <h1 className="text-4xl font-bold font-mono">Home Page</h1>
-    </div>
+    <div className="min-h-screen flex items-center justify-center bg-white-500 text-white">
+      <h1 className="text-4xl font-bold font-mono"></h1>
+      <img 
+        src="/scubacat.gif"
+        alt="Scuba cat animation"
+        className="w-96 h-auto"
+      />
     
+    </div>
   )
 }
 
